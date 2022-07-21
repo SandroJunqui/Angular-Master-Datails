fa0d6e97b2db08fd22f30f2d813f01657c4af1ff
SandroJunqui <sandrojunqui@gmail.com>
2022-07-21 11:59:57 -0300

    improving category list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'; // especifico para usar o in-memory -  excluir para conectar ao bd real
-import { InMemoryDatabase } from './in-memory-database'; // especifico para usar o in-memory -  excluir para conectar ao bd real
+import { InMemoryDatabase } from './in-memory-database';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +13,7 @@ import { InMemoryDatabase } from './in-memory-database'; // especifico para usar
     BrowserModule,
     AppRoutingModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase), // especifico para usar o in-memory -  excluir para conectar ao bd real
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,15 +1,35 @@
+import { CategoryModel } from './../shared/categoryModel';
+import { CategoryService } from './../shared/category.service';
+import { CategoriesModule } from './../categories.module';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.css']
+  styleUrls: ['./category-list.component.css'],
 })
 export class CategoryListComponent implements OnInit {
+  categories: CategoryModel[] = [];
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
+    this.categoryService.getAll().subscribe(
+      (categories) => (this.categories = categories),
+      (error) => alert('Erro ao carregar categorias!')
+    );
   }
 
+  deleteCategory(category: CategoryModel) {
+    const mustDelete = confirm('Deseja realmente excluir este item?');
+
+    if (mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        () =>
+          this.categories = this.categories.filter(
+            element => element != category),
+        () => alert('Erro ao tentar excluir')
+      )
+    }
+  }
 }
