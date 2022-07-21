47cf4cf001523cb108ddef051211c3044764a7fb
SandroJunqui <sandrojunqui@gmail.com>
2022-07-21 10:46:02 -0300

    intercepting http request with angular-in-memory-web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'; // especifico para usar o in-memory -  excluir para conectar ao bd real
+import { InMemoryDatabase } from './in-memory-database'; // especifico para usar o in-memory -  excluir para conectar ao bd real
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase), // especifico para usar o in-memory -  excluir para conectar ao bd real
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -4,69 +4,65 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryService {
+  private apiPath: string = 'api/categories';
 
-  private apiPath: string = "api/categories";
+  constructor(private http: HttpClient) {}
 
-constructor(private http: HttpClient) { }
+  getAll(): Observable<CategoryModel[]> {
+    return this.http.get(this.apiPath).pipe(
+      catchError(this.handlerError),
+      map(this.jsonDataToCategories) // retorno da api
+    );
+  }
 
-getAll(): Observable<CategoryModel[]> {
-  return this.http.get(this.apiPath).pipe(
-    catchError(this.handlerError),
-    map(this.jsonDataToCategories)  // retorno da api
-  )
-}
+  getById(id: number): Observable<CategoryModel> {
+    const url = `${this.apiPath}/${id}`;
+    return this.http
+      .get(url)
+      .pipe(catchError(this.handlerError), map(this.jsonDataToCategory));
+  }
 
-getById(id: number): Observable<CategoryModel> {
-  const url = `${this.apiPath}/${id}`;
-  return this.http.get(url).pipe(
-    catchError(this.handlerError),
-    map(this.jsonDataToCategory)
-  )
-}
+  create(category: CategoryModel): Observable<CategoryModel> {
+    return this.http
+      .post(this.apiPath, category)
+      .pipe(catchError(this.handlerError), map(this.jsonDataToCategory));
+  }
 
-create(category: CategoryModel): Observable<CategoryModel> {
-  return this.http.post(this.apiPath, category).pipe(
-    catchError(this.handlerError),
-    map(this.jsonDataToCategory)
-  )
-}
+  update(category: CategoryModel): Observable<CategoryModel> {
+    const url = `${this.apiPath}/${category.id}`;
 
-update(category: CategoryModel): Observable<CategoryModel> {
-  const url = `${this.apiPath}/${category.id}`;
+    return this.http.put(url, category).pipe(
+      catchError(this.handlerError),
+      map(() => category) // desta forma porque o put nao retorna dados
+    );
+  }
 
-  return this.http.put(url, category).pipe(
-    catchError(this.handlerError),
-    map(() => category) // desta forma porque o put nao retorna dados
-  )
-}
+  delete(id: number): Observable<any> {
+    const url = `${this.apiPath}/${id}`;
 
-delete(id: number): Observable<any> {
-  const url = `${this.apiPath}/${id}`;
+    return this.http.delete(url).pipe(
+      catchError(this.handlerError),
+      map(() => null)
+    );
+  }
 
-  return this.http.delete(url).pipe (
-    catchError(this.handlerError),
-    map(() => null)
-  )
-}
+  // PRIVATE METODOS
 
-// PRIVATE METODOS
+  private jsonDataToCategories(jsonData: any[]): CategoryModel[] {
+    const categories: CategoryModel[] = [];
+    jsonData.forEach((Element) => categories.push(Element as CategoryModel));
+    return categories;
+  }
 
-private jsonDataToCategories(jsonData: any[]): CategoryModel[] {
-  const categories: CategoryModel[] = [];
-  jsonData.forEach(Element => categories.push(Element as CategoryModel));
-  return categories;
-}
+  private jsonDataToCategory(jsonData: any): CategoryModel {
+    return jsonData as CategoryModel;
+  }
 
-private jsonDataToCategory(jsonData: any): CategoryModel {
-  return jsonData as CategoryModel;
-}
-
-private handlerError(error: any): Observable<any>{
-  console.log("ERRO NA REQUISIÇÃO => ", error);
-  return throwError(error);
-}
-
+  private handlerError(error: any): Observable<any> {
+    console.log('ERRO NA REQUISIÇÃO => ', error);
+    return throwError(error);
+  }
 }
